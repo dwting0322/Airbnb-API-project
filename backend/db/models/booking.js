d7731d624b22c170e2484743c7315bd8a7a598e7
david <dwting0322@gmail.com>
2022-08-01 20:06:57 -0700

    test

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Booking.belongsTo(models.User, {foreignKey: 'userId'})
       Booking.belongsTo(models.Spot, {foreignKey: 'spotId'})
+      
     }
   }
   Booking.init({
